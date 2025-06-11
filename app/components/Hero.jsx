130a701fff89873145acff50b981bbad503b8c10
XxAbrahamLincolnxX <asajernigan1894@gmail.com>
2025-06-11 11:17:54 -0700

    trying a header thing from tailwind

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='bg-cover bg-fixed bg-[url(../public/pawel-czerwinski-OY3slACa3ec-unsplash.jpg)] flex justify-center h-2/3 items-center'>
 
         <div className='max-h-1/4 w-1/2 text-center flex justify-center items-center'>
-        <div className='grid grid-rows-3 rounded-4xl items-center justify-center p-4 w-[500px] border-2 shadow-2xl bg-transparent backdrop-blur-lg'>
+        <div className='grid grid-rows-3 rounded-4xl items-center justify-center p-4 w-[500px] border-2 shadow-2xl bg-transparent backdrop-blur-lg md:w-[300px]'>
        <h2 className='text-6xl'>Hero Section</h2>
        <p>l;aksjdfalkjsdg;laksjdgl;aksjdgl;aksjdglaksdj</p>
        <button className='p-4 bg-gray-600 m-4 shadow-md'>Click Me</button>
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Body from '../app/components/Body';
 
 export default function Home() {
   return (
-    <div className="flex w-screen h-screen bg-amber-500">
+    <div className="flex bg-amber-500">
 
-      <div className='flex-1 h-screen relative'>
+      <div className='w-screen h-screen'>
       <Header />
       <Hero />
       <Body />
