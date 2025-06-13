767ecd16c89ba7ad281d92b81a94abdc882ad729
XxAbrahamLincolnxX <asajernigan1894@gmail.com>
2025-06-13 10:30:09 -0700

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import FollowCursor from "./components/FollowCursor";
 import Starfield from "./components/Starfield";
 
 
